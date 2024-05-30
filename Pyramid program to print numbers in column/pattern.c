36904eb9423f16c726427be05bc3e0e6264ea5bc
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-05-30 23:52:20 +0530

    Add files via upload

---
+++ Pyramid program to print numbers in column/pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main () {
+    int n;
+    printf("Enter the number of rows :");
+    scanf("%d",&n);
+
+    for(int i=1 ;i<=n;i++){
+        for(int k=1; k<=n-i ; k++){
+            printf(" ");
+            }
+            for(int j=1;j<=i;j++){
+                printf("%d ",j);
+            }
+            printf("\n");
+    }
+return 0;
+}
+
+/*
+output :
+Enter the number of rows :5
+    1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+*/
