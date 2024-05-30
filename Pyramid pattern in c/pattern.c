2be929b1af5bc00d4a9e094f11ec0592b1719f0d
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-05-30 23:31:53 +0530

    Add files via upload

---
+++ Pyramid pattern in c/pattern.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main () {
+    int n;
+    printf("Enter the number of rows :");
+    scanf("%d",&n);
+
+    for(int i=1 ;i<=n;i++){
+        for(int k=1; k<=n-i ; k++){
+            printf(" ");
+            }
+            for(int j=1;j<=i;j++){
+                printf("* ");
+            }
+            printf("\n");
+    }
+
+
+
+ /*
+Enter the number of rows :5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ */
+
+
+
+
+    return 0;
+}
