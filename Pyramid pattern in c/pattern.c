77de12d5b7c4bdd1adbda4d65ec01e4aed0c072b
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-12 23:37:11 +0530

    pattern.c

--- Pyramid pattern in c/pattern.c
+++ Pyramid pattern in c/pattern.c
@@ -4,7 +4,7 @@ int main () {
     int n;
     printf("Enter the number of rows :");
     scanf("%d",&n);
-  
+    
     for(int i=1 ;i<=n;i++){
         for(int k=1; k<=n-i ; k++){                       //This loop is for spacing
             printf(" ");
